c80829de37453cc0e0daf9d831d05454c28bcce3
Ivan Besarab <Ivan.Besarab@RockSoft.rocks>
2018-07-13 18:42:34 +0300

    Initial Commit

---
+++ ReadFromPlist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReadFromPlist
+//
+//  Created by Ivan Vasilevich on 7/13/18.
+//  Copyright © 2018 RockSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
